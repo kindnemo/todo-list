52b4b52351cd8c89b2b826ef71423ec485a18557
kindnemo <namanmishraepicstar@gmail.com>
2021-12-28 00:49:36 +0530

    Added the check button besides the task will add the functionality tomorrow

--- script.js
+++ script.js
@@ -4,11 +4,32 @@ const taskInputDiv = document.querySelector("#add-task-div");
 const taskAdd = document.querySelector("#task-add");
 const taskAddPop = document.querySelectorAll(".task-add-btn");
 const cancel = document.querySelector("#cancel");
+const date = document.querySelector("#date");
+
+date.textContent = new Date().toDateString().slice(0,11);
+
+
+//CREATING THE CHECK MARK BUTTON
+let checkBtn = document.createElement("BUTTON");
+checkBtn.setAttribute("id","check-btn");
+let checkSpan = document.createElement("SPAN");
+checkSpan.setAttribute("id","check-mark-span");
+let checkImage = document.createElement("IMG");
+checkImage.src = "images/check-mark.png";
+checkImage.alt = "check-mark-image";
+checkImage.setAttribute("id","check-mark-image");
+checkSpan.appendChild(checkImage);
+checkBtn.appendChild(checkSpan);
 
 
 function addTask(){
     let taskToAdd = document.createElement("LI");
-    taskToAdd.textContent = taskInput.value;
+    let textSpan = document.createElement("SPAN");
+    
+    textSpan.textContent = taskInput.value;
+    taskToAdd.appendChild(checkBtn);
+    taskToAdd.appendChild(textSpan);
+    
     tasks.appendChild(taskToAdd);
     taskInput.value="";
 }
@@ -24,7 +45,6 @@ function cancelTask(){
     taskAddPop[1].classList.toggle("display-toggle");
     taskAddPop[0].addEventListener("click", taskInputDisplay);
 }
-
 
 
 
